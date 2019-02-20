a4a91b46ac120b307963349d1df0d1cc0cb04aa5
Fernando F. Gallego <fernando.gallego@tado.com>
2019-02-20 14:14:14 +0100

    Add files

---
+++ CircularRevealFragmentExample/app/src/main/java/com/fernandofgallego/circularrevealfragmentexample/OnFragmentTouched.java
@@ -0,0 +1,11 @@
+package com.fernandofgallego.circularrevealfragmentexample;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by fernando.gallego on 11/11/14.
+ */
+public interface OnFragmentTouched {
+    public void onFragmentTouched(Fragment fragment, float x, float y);
+}
